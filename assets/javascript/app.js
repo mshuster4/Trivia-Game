3c5f9d796bae8c49c4c9468e6a2b6151dcec3410
Emroze <emrozesd@users.noreply.github.com>
2018-12-16 20:22:20 -0800

    psuedocode added

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+//Display start page with start button when page loads or game resets 
+//When button is pressed, display 10 sec timer along with muliple choice question 
+//If the user presses the correct answer the timer stops and the "correct answer" image is displayed
+//If the user presses the incorrect answer or time runs out, the "nope" image is display 
+//Game moves onto the next question a few seconds afterward with no prompting 
+//After all questions are done, display correct answers, incorrect answers, and unanswered questions
+//Display "Start over?"
+//Reset game (with no page reload)
+
