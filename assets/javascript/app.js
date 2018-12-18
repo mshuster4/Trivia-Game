9bcc63af3a08d16e39d509847dd5ec022c80014b
Emroze <emrozesd@users.noreply.github.com>
2018-12-17 18:17:16 -0800

    Added ten second timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,3 +7,60 @@
 //Display "Start over?"
 //Reset game (with no page reload)
 
+var trivia = [
+    questionsObject = {
+        qOne: 'Which type of animal is Rocko from Rockos Modern Life?',
+        qTwo: 'What is the name of Rugrat Angelicas doll?',
+        qThree: 'Which school do the kids in Hey Arnold attend?',
+    },
+    
+    optionsObject = {
+        optionsOne : ['Dog', 'Wallaby', 'Mouse', 'Beaver'],
+        optionsTwo: ['Sue', 'Cynthia', 'Betty', 'Candy'],
+        optionThree: ['PS 118', 'PS 108', 'PS 110', 'PS 128'],
+    },
+
+    answersObject = {
+        ansOne: ['Wallaby'],
+        ansTwo: ['Cynthia'],
+        ansThree: ['PS 118'], 
+        
+    },
+];
+
+var isTimerRunning = false; 
+
+var intervalID;
+
+var time = 10
+
+console.log(time); 
+
+function startTimer() {
+
+    if (!isTimerRunning) {
+        intervalID = setInterval(count, 1000);
+        isTimerRunning = true; 
+    }
+
+}
+
+function stopTimer() {
+
+    clearInterval(intervalID);
+    isTimerRunning = false; 
+
+}
+
+function count() {
+
+    time --;
+    console.log(time);
+    if (time === 0) {
+        stopTimer();
+    }
+
+}
+
+$("#start-button").on('click', startTimer); 
+
